ad13598bff695b644c7e9fb3604900984b2c8312
homeHara <bizhara@gmail.com>
2017-08-26 19:11:03 +0900

    Just add framework project.

---
+++ UIViewFrameForIB/UIViewFrameForIB/UIViewFrameForIB.h
@@ -0,0 +1,19 @@
+//
+//  UIViewFrameForIB.h
+//  UIViewFrameForIB
+//
+//  Created by hara on 2017/08/26.
+//  Copyright © 2017年 hara. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UIViewFrameForIB.
+FOUNDATION_EXPORT double UIViewFrameForIBVersionNumber;
+
+//! Project version string for UIViewFrameForIB.
+FOUNDATION_EXPORT const unsigned char UIViewFrameForIBVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UIViewFrameForIB/PublicHeader.h>
+
+
